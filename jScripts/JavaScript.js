ecb9912ab0ddc3fc2ca6b9a83c331ccaafc8ef93
shahrH30 <134510671+shahrH30@users.noreply.github.com>
2023-06-06 13:44:33 +0300

    Add files via upload

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,48 @@
+﻿
+let slideIndex =1;
+let bigImages, smallImages;
+
+function opengallery() {
+    bigImages = document.getElementsByClassName("pilatesImages");
+    smallImages = document.getElementsByClassName("showSmallImage");
+
+    for (let i = 0; i < bigImages.length; i++) {
+        bigImages[i].style.display = "none";
+    }
+
+    /*    האם אני צריכה כי גם הCSS עושה את הפעולה של ההסתרה פה זה רק מוודא שאין משהו פעיל*/
+   /*לא רלוונטי בקוד הספציפי שלנו כי הגדרנו שכולם שכל התמונות יהיו עם אופסיטי אבל אם תהיה תמונה שלא תהיה עם עיצוב התחלתי בשקיפות אז הלולאה תשנה לה את השקיפות */
+    //for (let i = 0; i < smallImages.length; i++) {
+    //    smallImages[i].className = smallImages[i].className.replace(" active", "");
+    //}
+
+    showImage(slideIndex);
+}
+
+// מקדם את המיקום של מספר התמונה בשימוש בחצים // 
+function changeImage(n) {
+    showImage(slideIndex + n);
+}
+
+//מעדכן את מספר התמונה הרלוונטית בזמן לחיצה עליה//
+function currentSlide(n) {
+    showImage(n);
+}
+
+
+function showImage(n) {
+    let captionText = document.getElementById("caption");
+
+    //מאפסים את התמונה הנוכחית שמוצגת נאן הופך אותה ללא נראת ואאקטיב 
+    bigImages[slideIndex - 1].style.display = "none";
+    smallImages[slideIndex - 1].className = smallImages[slideIndex - 1].className.replace(" active", "");
+
+    slideIndex = n;
+    //מאפס אם הגענו לקצה//
+    if (slideIndex > bigImages.length) { slideIndex = 1 }
+    if (slideIndex < 1) { slideIndex = bigImages.length }
+
+    bigImages[slideIndex - 1].style.display = "block";
+    smallImages[slideIndex - 1].className += " active";
+    captionText.innerHTML = smallImages[slideIndex - 1].alt;
+}
